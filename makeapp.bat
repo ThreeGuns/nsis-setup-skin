83fd8acbfcfff04578d59ce18dacb20b4a8d1693
cailong <cailong1028@126.com>
2018-06-20 17:01:34 +0800

    安装统计

--- makeapp.bat
+++ makeapp.bat
@@ -6,6 +6,8 @@ del ".\SetupScripts\app.7z"
 
 rem 生成app.7z
 7z.exe a ".\SetupScripts\app.7z" ".\%destPathName%\*.*"
+7z.exe a ".\SetupScripts\app.7z" ".\SetupScripts\installStatistics.bat"
+7z.exe a ".\SetupScripts\app.7z" ".\SetupScripts\curl-7.60.0\AMD64\*.*"
 
 @set DestPath=%cd%\%destPathName%\
 @echo off& setlocal EnableDelayedExpansion
